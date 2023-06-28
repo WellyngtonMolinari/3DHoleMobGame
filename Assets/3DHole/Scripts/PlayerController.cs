f0c1bb7510d6e227512e933dd89b375307bba734
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-06-28 14:36:52 -0300

    Added movement controls

--- Assets/3DHole/Scripts/PlayerController.cs
+++ Assets/3DHole/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header(" Settings ")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float screenPositionFollowThreshold;
+    private Vector3 clickedScreenPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,38 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ManageControl();
+    }
+
+    private void ManageControl()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickedScreenPosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            // First calculate the difference in screen position
+            Vector3 difference = Input.mousePosition - clickedScreenPosition;
+
+            Vector3 direction = difference.normalized;
+
+            float maxScreenDistance = screenPositionFollowThreshold * Screen.height;
+
+            if (difference.magnitude > maxScreenDistance)
+            {
+                clickedScreenPosition = Input.mousePosition - direction * maxScreenDistance;
+                difference = Input.mousePosition - clickedScreenPosition;
+            }
+
+            difference /= Screen.width;
+
+            difference.z = difference.y;
+            difference.y = 0;
+
+            Vector3 TargetPosition = transform.position + difference * moveSpeed * Time.deltaTime;
+
+            transform.position = TargetPosition;
+        }
     }
 }
