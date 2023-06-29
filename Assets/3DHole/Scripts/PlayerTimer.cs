f8222b523b1b3eebc528be6ca93f0b2d35c9f729
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-06-29 15:52:42 -0300

    Added Timer countdown function

--- Assets/3DHole/Scripts/PlayerTimer.cs
+++ Assets/3DHole/Scripts/PlayerTimer.cs
@@ -1,18 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerTimer : MonoBehaviour
 {
+    [Header(" Elements ")]
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private int timerDuration;
+
+    private int timer;
+    private bool timerIsOn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartTimer();
+        }
+    }
+
+    private void Initialize()
+    {
+        timer = timerDuration;
+        timerText.text = FormatSeconds(timer);
+    }
+
+    public void StartTimer()
+    {
+        if (timerIsOn)
+        {
+            Debug.LogWarning("A timer is already on");
+            return;
+        }
         
+        Initialize();
+
+        timerIsOn = true;
+
+        StartCoroutine(TimerCoroutine());
+    }
+
+    IEnumerator TimerCoroutine()
+    {
+        while (timerIsOn)
+        {
+            yield return new WaitForSeconds(1);
+
+            timer--;
+            timerText.text = FormatSeconds(timer);
+
+            if (timer == 0)
+            {
+                timerIsOn = false;
+                StopTimer();
+            }
+
+        }
+    }
+
+    public void StopTimer()
+    {
+        Debug.Log("Timer is over!");
+    }
+
+    private string FormatSeconds(int totalSeconds)
+    {
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+
+        return minutes.ToString("D2") + ":" + seconds.ToString("D2");
     }
 }
