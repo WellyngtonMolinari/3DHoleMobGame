48c91bf3bec396c389852c2754867209b84de5e4
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-06-29 09:00:02 -0300

    Added new fill amount bar

--- Assets/3DHole/Scripts/PlayerSize.cs
+++ Assets/3DHole/Scripts/PlayerSize.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerSize : MonoBehaviour
 {
+    [Header (" Elements ")]
+    [SerializeField] private Image fillImage;
+
     [Header(" Settings ")]
-    [Tooltip("Increase the limit of blocks eaten to increase the size")]
+    [Tooltip("Increase the limit of size of blocks eaten to increase the size")]
     [SerializeField] private float scaleIncreaseThreshold;
     [Tooltip("Increase the size of the player multiplier")]
     [SerializeField] private float scaleStep;
     private float scaleValue;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        fillImage.fillAmount = 0;
     }
 
     // Update is called once per frame
@@ -37,5 +42,12 @@ public class PlayerSize : MonoBehaviour
             IncreaseScale();
             scaleValue = scaleValue % scaleIncreaseThreshold;
         }
+
+        UpdateFillDisplay();
+    }
+    
+    private void UpdateFillDisplay()
+    {
+        fillImage.fillAmount = scaleValue / scaleIncreaseThreshold;
     }
 }
