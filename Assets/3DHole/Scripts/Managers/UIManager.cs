5aad409f3207d83f2bc00ebc55823ebdeb987c61
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-06-30 19:45:55 -0300

    Useless libraries

--- Assets/3DHole/Scripts/Managers/UIManager.cs
+++ Assets/3DHole/Scripts/Managers/UIManager.cs
@@ -1,9 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
-using UnityEngine.Events;
+
 
 public class UIManager : MonoBehaviour
 {
