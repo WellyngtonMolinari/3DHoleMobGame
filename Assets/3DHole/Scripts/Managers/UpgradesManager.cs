1b5bd07ab43e53f4147151e79ee1bf9d19036e60
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-06-30 02:43:07 -0300

    Added references for the Texts of Upgrades Cards

--- Assets/3DHole/Scripts/Managers/UpgradesManager.cs
+++ Assets/3DHole/Scripts/Managers/UpgradesManager.cs
@@ -2,24 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class UpgradesManager : MonoBehaviour
 {
-    [Header (" Events ")]
+
+    [Header(" Elements ")]
+    [SerializeField] private Button timerButton;
+    [SerializeField] private Button sizeButton;
+    [SerializeField] private Button powerButton;
+
+    [Header(" Data ")]
+    private int timerLevel;
+    private int sizeLevel;
+    private int powerLevel;
+
+
+    private const string timerKey = "TimerLevel";
+    private const string sizeKey = "SizeLevel";
+    private const string powerKey = "PowerLevel";
+
+
+    [Header(" Events ")]
     public static Action onTimerPurchased;
     public static Action onSizePurchased;
     public static Action onPowerPurchased;
 
+    private void Awake()
+    {
+        LoadData();
+        InitializeButtons();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    private void InitializeButtons()
+    {
+        // A script attached to each upgrade button
+        // Call this script and configure
+        // Upgrade level
+        // Interactability
+
+        int timerPrice = 100;
+        int sizePrice = 150;
+        int powerPrice = 300;
         
+        timerButton.GetComponent<UpgradeButton>().Configure(timerLevel, timerPrice);
+        sizeButton.GetComponent<UpgradeButton>().Configure(sizeLevel, sizePrice);
+        powerButton.GetComponent<UpgradeButton>().Configure(powerLevel, powerPrice);
     }
 
     public void TimerButtonCallback()
@@ -35,6 +74,20 @@ public class UpgradesManager : MonoBehaviour
     public void PowerButtonCallback()
     {
         onPowerPurchased?.Invoke();
+    }
+
+    private void LoadData()
+    {
+        timerLevel = PlayerPrefs.GetInt(timerKey);
+        sizeLevel = PlayerPrefs.GetInt(sizeKey);
+        powerLevel = PlayerPrefs.GetInt(powerKey);
+    }
+
+    private void SaveData()
+    {
+        PlayerPrefs.SetInt(timerKey, timerLevel);
+        PlayerPrefs.SetInt(sizeKey, sizeLevel);
+        PlayerPrefs.SetInt(powerKey, powerLevel);
     }
 
 }
