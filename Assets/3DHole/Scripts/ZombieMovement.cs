ddb73dc69a4082de5f7e0ae1dc4f207ea77cbe0f
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-07-03 19:35:23 -0300

    Added new zombie patroling

---
+++ Assets/3DHole/Scripts/ZombieMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class ZombieMovement : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 10f;
+
+    private int currentWaypointIndex = 0;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (waypoints.Length == 0)
+            return;
+
+        Vector3 targetPosition = waypoints[currentWaypointIndex].position;
+        Vector3 movementDirection = (targetPosition - transform.position).normalized;
+        Vector3 movement = movementDirection * movementSpeed;
+
+        rb.velocity = movement;
+
+        Quaternion targetRotation = Quaternion.LookRotation(movementDirection);
+        rb.rotation = Quaternion.Slerp(rb.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
+        {
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= waypoints.Length)
+                currentWaypointIndex = 0;
+        }
+        
+    }
+
+    
+}
