807a149e2a19319293e2d55601faf4d0d2960c2e
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-07-05 12:09:12 -0300

    Added new Eye following system

--- Assets/3DHole/Scripts/EyeController.cs
+++ Assets/3DHole/Scripts/EyeController.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EyeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header(" Settings ")]
+    [SerializeField] private float moveSpeed;
+    private Vector3 clickedScreenPosition;
 
     // Update is called once per frame
     void Update()
     {
-        
+        ManageControl();
+    }
+
+    private void ManageControl()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickedScreenPosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            // First calculate the difference in screen position
+            Vector3 difference = Input.mousePosition - clickedScreenPosition;
+
+            Vector3 direction = difference.normalized;
+
+            float maxScreenDistance = Screen.height;
+
+            if (difference.magnitude > maxScreenDistance)
+            {
+                clickedScreenPosition = Input.mousePosition - direction * maxScreenDistance;
+                difference = Input.mousePosition - clickedScreenPosition;
+            }
+
+            difference /= Screen.width;
+
+            difference.z = difference.y;
+            difference.y = 0;
+
+            Vector3 TargetPosition = transform.position + difference * moveSpeed * Time.deltaTime;
+
+            transform.position = TargetPosition;
+        }
     }
 }
